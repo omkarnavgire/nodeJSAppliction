0d055486a242c537f808a84d6a61927b557b086f
omkarnavgire <omkarnavgire55@gmail.com>
2026-06-30 12:40:34 +0530

    Updated Node.js application

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 
+const PORT = process.env.PORT || 8080;
+
 app.get('/', (req, res) => {
-    res.send('Hello this is  Node js!');
+    res.send('Hello this is Node.js!');
 });
 
-app.listen(3000, () => {
-    console.log('Server running on port 3000');
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
