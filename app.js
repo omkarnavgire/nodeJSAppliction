845108f804c3ee35c1137e28f6b0cc32b97e8d92
omkarnavgire <omkarnavgire55@gmail.com>
2026-06-30 12:51:45 +0530

    Updated Node.js application

--- app.js
+++ app.js
@@ -4,7 +4,18 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-    res.send('Hello this is Node.js!');
+    res.send(`
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>Node.js App</title>
+        </head>
+        <body style="font-family: Arial; text-align:center; margin-top:100px;">
+            <h2>Today's date: ${new Date().toLocaleDateString()}</h2>
+            <h1>Welcome to Node.js App</h1>
+        </body>
+        </html>
+    `);
 });
 
 app.listen(PORT, () => {
